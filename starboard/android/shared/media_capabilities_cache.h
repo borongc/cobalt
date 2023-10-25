f8bf16b9732638240c582e164468bf6b1e29057f
xiaomings <xiaomings@google.com>
2023-10-25 15:35:00 -0700

    [android] Improve multi-encrypted-video demo (#1837)
    In Cobalt:
    * Removed tunnel mode check on audio decoder in Cobalt.
    The check prevents tunnel mode from being enabled via mime attribute,
    which is required to enable tunnel mode on the primary video of the
    demo, as mentioned below. The previous implementation checks whether the
    audio decoder supports tunnel mode before enabling it. However, none of
    them does, and the audio will be decoded to PCM before sending to the
    AudioTrack, so whether the audio decoder supports tunnel mode is
    irrelevant.
    
    In the demo:
    1. Now the demo tries to play the primary video using tunnel mode, by
    passing "tunnelmode=true" as an extra mime attribute when supported.
    2. Added more streams with different codecs and resolutions, and allow
    specifying them at runtime.
    3. Try to create com.widevine.alpha when creation of
    com.youtube.widevine.l3 fails. This allows the demo to run on browsers
    without Widevine L3 support, including Chrome.
    4. Download media data at startup to reduce CPU usage durnig playback.
    
    b/175883701
    
    Change-Id: Id1e6c3f19a509f20b8f6413840b5ad223e5437ee

--- starboard/android/shared/media_capabilities_cache.h
+++ starboard/android/shared/media_capabilities_cache.h
@@ -126,9 +126,7 @@ class MediaCapabilitiesCache {
 
   int GetMaxAudioOutputChannels();
 
-  bool HasAudioDecoderFor(const std::string& mime_type,
-                          int bitrate,
-                          bool must_support_tunnel_mode);
+  bool HasAudioDecoderFor(const std::string& mime_type, int bitrate);
 
   bool HasVideoDecoderFor(const std::string& mime_type,
                           bool must_support_secure,
@@ -139,9 +137,7 @@ class MediaCapabilitiesCache {
                           int bitrate,
                           int fps);
 
-  std::string FindAudioDecoder(const std::string& mime_type,
-                               int bitrate,
-                               bool must_support_tunnel_mode);
+  std::string FindAudioDecoder(const std::string& mime_type, int bitrate);
 
   std::string FindVideoDecoder(const std::string& mime_type,
                                bool must_support_secure,
